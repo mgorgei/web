6955c4bc78e8b93023b7098134ef458ad85619be
Michael Gorgei <mgorgei@umail.iu.edu>
2015-03-25 14:22:25 -0500

    Cleaned up the php file and validated data
    There's also a header that should signal the $ajax error handler on
    client side when something goes wrong.

--- ajax.php
+++ ajax.php
@@ -1,62 +1,55 @@
 <?php
 include 'pwds.php';
-if ($_SERVER["REQUEST_METHOD"] === 'GET' and $_SERVER["QUERY_STRING"] === 'timers') {
-	try {
-		$dbh = new PDO($dsn, $username, $password, $options);
-		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+/*my free server host won't play nice with DELETE / PUT / etc. requests (need a
+  workaround) -> first character of the query string indicates what the
+  original request was supposed to be in CRUD.
+  CRUD in a GET request
+  C == Create 
+  R == Read (already correctly handled by GET)
+  U == Update
+  D == Delete
+*/
+try {
+	$dbh = new PDO($dsn, $username, $password, $options);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+	echo 'failed fetch';
+	echo $e->getMessage();
+	die('why you die');
+}
+if ($_SERVER["REQUEST_METHOD"] === 'GET') {
+	//Read request R == Read
+	if (substr($_SERVER["QUERY_STRING"], 0, 1) === 'R') {
 		$sql = 'SELECT id, type, time FROM `Timer`';
 		foreach ($dbh->query($sql) as $row) {
 			echo $row['id'] . "\t";
 			echo $row['type'] . "\t";
 			echo $row['time'] . "\n";
 		}
-		$query = $dbh->prepare($sql);
-		$query->execute();
-		$rows = $query->fetchAll();
-		foreach ($rows as $rs) {
-			$id = $rs['id'];
+	}
+	//Create request C == Create
+	elseif (substr($_SERVER["QUERY_STRING"], 0, 1) === 'C') {
+		$input = explode(',', substr($_SERVER["QUERY_STRING"], 1));//typical value-> C17:10:00,0
+		if (is_numeric(str_replace(':', '', $input[0])) and is_numeric($input[1])) {
+			$sql = 'INSERT INTO `Timer` (`time`, `type`) VALUES (\'' . $input[0] . '\', \'' . $input[1] . '\');';
+			$dbh->query($sql);
+			//getting the MAX id should get the latest created auto increment primary key value to pass back to the application
+			$sql = 'SELECT MAX(id) as \'m\' FROM `Timer`';
+			foreach ($dbh->query($sql) as $row)
+				echo $row['m'];
 		}
 	}
-	catch(PDOException $e) {
-		echo 'failed fetch';
-		echo $e->getMessage();
-		die('why you die');
+	//Delete request D == Delete
+	elseif (substr($_SERVER["QUERY_STRING"], 0, 1) === 'D') {
+		if (is_numeric(substr($_SERVER["QUERY_STRING"], 1))) {
+			$sql = 'DELETE FROM `Timer` WHERE id=' . substr($_SERVER["QUERY_STRING"], 1);
+			$dbh->query($sql);
+		}
 	}
 }
-
-//cannot use DELETE right now on server...
-if ($_SERVER["REQUEST_METHOD"] === 'GET' and substr($_SERVER["QUERY_STRING"], 0, 1) === 'D') {// and is_numeric($_SERVER["QUERY_STRING"])) {
-	try {
-		$dbh = new PDO($dsn, $username, $password, $options);
-		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = 'DELETE FROM `Timer` WHERE id=' . substr($_SERVER["QUERY_STRING"], 1);
-		$dbh->query($sql);
-	}
-	catch(PDOException $e) {
-		echo 'failed deletion';
-		echo $e->getMessage();
-		die('why you die');
-	}
+else {
+	header("HTTP/1.1 406 Not Acceptable");
+	exit();
 }
-
-if ($_SERVER["REQUEST_METHOD"] === 'GET' and substr($_SERVER["QUERY_STRING"], 0, 1) === 'P') {//<-- really need to validate data from XSS on this one...
-	try {
-		$dbh = new PDO($dsn, $username, $password, $options);
-		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$input = explode(',', substr($_SERVER["QUERY_STRING"], 1));
-		$sql = 'INSERT INTO `Timer` (`time`, `type`) VALUES (\'' . $input[0] . '\', \'' . $input[1] . '\');';
-		//echo $_SERVER["QUERY_STRING"];
-		//echo $sql;
-		$dbh->query($sql);
-		$sql = 'SELECT MAX(id) as \'m\' FROM `Timer`';
-		foreach ($dbh->query($sql) as $row)
-			echo $row['m'];
-	}
-	catch(PDOException $e) {
-		echo 'failed post';
-		echo $e->getMessage();
-		die('why you die');
-	}
-}
-#echo phpinfo();//this is useful server info
 ?>
