cdb02447f5ac71bc74f28fd3b16220868db1fc7e
Michael Gorgei <mgorgei@umail.iu.edu>
2015-04-08 16:49:02 -0500

    Hyperlink responses + using prepared SQL
    still need to make sure user input is XSS safe somehow

--- ajax.php
+++ ajax.php
@@ -3,13 +3,12 @@ include 'pwds.php';
 /*connect to the MySQL database on the server.  Respond based on the post 
   request type, the first object's name, and the first key of the expected JSON
   object.  
-*/
+******************************************************************************/
 try {
 	$dbh = new PDO($dsn, $username, $password, $options);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
-	echo 'failed fetch';
 	echo $e->getMessage();
 	die('why you die');
 }
@@ -22,6 +21,7 @@ else {
 }
 $js = json_decode(rawurldecode($data));
 
+/*****************************************************************************/
 //deal with timers
 if (isset($js->timers)) {
 	//GET READ //{"timers":[{"READ":"all"}]}
@@ -41,11 +41,12 @@ if (isset($js->timers)) {
 	elseif ($_SERVER["REQUEST_METHOD"] === 'POST' and isset($js->timers[0]->CREATE)) {
 		if (is_numeric(str_replace(':', '', $js->timers[0]->CREATE))) {
 			//should check if the time is valid instead of just numeric!!!
-			$sql = 'INSERT INTO `Timer` (`time`, `type`) VALUES (\'' . $js->timers[0]->CREATE . '\', \'' . 0 . '\');';
-			$dbh->query($sql);
+			$data = array($js->timers[0]->CREATE, 0);
+			$STH = $dbh->prepare('INSERT INTO `Timer` (`time`, `type`) VALUES (?, ?)');
+			$STH->execute($data);
 			//getting the MAX id should get the latest created auto increment primary key value to pass back to the application
-			$sql = 'SELECT MAX(id) as \'m\' FROM `Timer`';
-			$out = $dbh->query($sql)->fetch()['m'];
+			$sql = 'SELECT MAX(id) FROM `Timer`';
+			$out = $dbh->query($sql)->fetch()['MAX(id)'];
 			echo json_encode( array('timers' => array(array('id' => $out ))));//array of array... well looks pretty wrong now, looked okay on JS side
 			exit();
 		}
@@ -53,46 +54,83 @@ if (isset($js->timers)) {
 	//DELETE DELETE //{"timers":[{"DELETE":"id"}]}
 	elseif ($_SERVER["REQUEST_METHOD"] === 'DELETE' and isset($js->timers[0]->DELETE)) {
 		if (is_numeric($js->timers[0]->DELETE)) {
-			$sql = 'DELETE FROM `Timer` WHERE id=' . $js->timers[0]->DELETE;
-			$dbh->query($sql);
+			$STH = $dbh->prepare('DELETE FROM `Timer` WHERE id=?');
+			$STH->execute(array($js->timers[0]->DELETE));
 			exit();
 		}
 	}
 }
+/*****************************************************************************/
 //deal with hyperlinks
 elseif (isset($js->hyper)) {
 	//GET READ //{"hyper":[{"READ":"all"}]}
-	print_r($js);
 	if ($_SERVER["REQUEST_METHOD"] === 'GET' and isset($js->hyper[0]->READ)) {
-		echo 'hyper get';
+		$sql = 'SELECT id, name, description, address, image, linkOrder FROM `Hyperlink`';
+		foreach ($dbh->query($sql) as $row) {
+			$out['id'] = $row['id'];
+			$out['name'] = $row['name'];
+			$out['description'] = $row['description'];
+			$out['address'] = $row['address'];
+			$out['image'] = $row['image'];
+			$out['linkOrder'] = $row['linkOrder'];
+			$output[] = $out;//push the array on
+		}
+		$output = array('hyper' => $output);
+		echo json_encode($output);
+		exit();
 	}
 	//POST CREATE //{"hyper":[{"CREATE":"link"}]}
 	elseif ($_SERVER["REQUEST_METHOD"] === 'POST' and isset($js->hyper[0]->CREATE)) {
-		echo 'hyper post';
+		if (true) {//presume XSS-checking is going on for now...
+			$data = array($js->hyper[0]->name, $js->hyper[0]->description, $js->hyper[0]->address, $js->hyper[0]->image, $js->hyper[0]->linkOrder);
+			$STH = $dbh->prepare('INSERT INTO `Hyperlink` (`name`, `description`, `address`, `image`, `linkOrder`) VALUES (?, ?, ?, ?, ?)');
+			$STH->execute($data);
+			//get the last inserted auto increment id
+			$sql = 'SELECT MAX(id) FROM `Hyperlink`';
+			$out = $dbh->query($sql)->fetch()['MAX(id)'];
+			echo json_encode( array('hyper' => array(array('id' => $out ))));
+			exit();
+		}
 	}
 	//PUT UPDATE //{"hyper":[{"UPDATE":"empty"}]}
 	elseif ($_SERVER["REQUEST_METHOD"] === 'PUT' and isset($js->hyper[0]->UPDATE)) {
-		echo 'hyper put';
-		/*  if swapped_position < highlight_value
-		UPDATE Hyperlink SET order = order - 1 WHERE order > hightlight_value and order <= swapped_position
-		else
-		UPDATE Hyperlink SET order = order + 1 WHERE order >= swapped_position AND order < highlighted_value
-		UPDATE Hyperlink SET order = swapped_position*/
+		if (true) {//presume XSS-checking is going on for now...
+			//the active item is being placed at a position more than itself
+			if ($js->hyper[0]->swap < $js->hyper[0]->active) {
+				$data = array($js->hyper[0]->active, $js->hyper[0]->swap);
+				$sql = 'UPDATE `Hyperlink` SET linkOrder = linkOrder - 1 WHERE linkOrder > ? AND linkOrder <= ?';
+			}
+			//the active item is being placed at a position less than itself
+			else {
+				$data = array($js->hyper[0]->swap, $js->hyper[0]->active);
+				$sql = 'UPDATE `Hyperlink` SET linkOrder = linkOrder + 1 WHERE linkOrder >= ? AND linkOrder < ?';
+			}
+			//moves items around to compensate for the shift
+			$STH = $dbh->prepare($sql);
+			$STH->execute($data);
+			//update the active hyperlink with the new value
+			$data = array($js->hyper[0]->swap, $js->hyper[0]->UPDATE);
+			$STH = $dbh->prepare('UPDATE `Hyperlink` SET linkOrder = ? WHERE id = ?');
+			$STH->execute($data);
+			exit();
+		}
 	}
 	//DELETE DELETE //{"hyper":[{"DELETE":"id"}]}
 	elseif ($_SERVER["REQUEST_METHOD"] === 'DELETE' and isset($js->hyper[0]->DELETE)) {
-		echo 'hyper delete';
+		if (is_numeric($js->hyper[0]->DELETE)) {
+			//move all with linkOrder > than the selected's linkOrder back
+			$STH = $dbh->prepare('UPDATE Hyperlink SET linkOrder = linkOrder - 1 WHERE linkOrder > ' .
+								 '(SELECT * from (SELECT linkOrder FROM Hyperlink WHERE id = ?) as t)');
+			$STH->execute(array($js->hyper[0]->DELETE));
+			//now delete
+			$STH = $dbh->prepare('DELETE FROM `Hyperlink` WHERE id=?');
+			$STH->execute(array($js->hyper[0]->DELETE));
+			exit();
+		}
 	}
-	exit();
-	
-
-/*`Hyperlink` (
-  `id` int(11)  AUTO_INCREMENT,
-  `name` tinytext 
-  `description` tinytext NOT NULL,
-  `address` tinytext 
-  `image` tinytext NOT NULL,*/
 }
+
+//will closing the session help eliminate double refresh = real problem?
 //did not recognize the JSON given
 else {
 	header("HTTP/1.1 402 Payment required");
